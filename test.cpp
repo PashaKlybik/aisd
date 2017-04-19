5c60d7f6316650a6b8f060486c0869b9f2629062
PashaKlybik <pashaklybik@gmail.com>
2017-04-19 03:31:39 +0300

    add vector

--- test.cpp
+++ test.cpp
@@ -1,9 +1,18 @@
 #include <iostream>
+#include <vector>
+
+using namespace std;
 
 int main() {
     int a, b;
     cin >> a >> b;
     cout >> a + b
+    std::vector<int> v;
+    for (int i =0; i<3; i++) {
+      int t;
+      cin >> t;
+      v.push_back(t);
+    }
     cout << "test we in master branch";
     return 0;
 }
